3d5cce9fcda67d367ff9f9ee14c531dfb552bed7
Kavarera <rafli.030715@gmail.com>
2024-07-09 19:34:44 +0700

    update transaction service

--- lib/core/services/transaction_service.dart
+++ lib/core/services/transaction_service.dart
@@ -4,24 +4,25 @@ import '../api/api_config.dart';
 import 'package:http/http.dart' as http;
 
 class TransactionService {
-  Future<bool> createTransaction(
+  Future<String> createTransaction(
       String token, int userId, int productId) async {
     final url = Uri.parse('${ApiConfig.baseUrl + ApiConfig.transaction}');
     try {
       final http.Response response = await http.post(url,
           headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer $token'
+            'Content-Type': 'application/json; charset=UTF-8'
           },
-          body: jsonEncode(<String, int>{
+          body: jsonEncode(<String, dynamic>{
             'customerId': userId,
             'productId': productId,
             "amount": 1,
+            "type": "QR"
           }));
       print('Response status code: ${response.statusCode}');
       print('Response body: ${response.body}');
       if (response.statusCode == 201) {
-        return true;
+        Map<String, dynamic> data = jsonDecode(response.body);
+        return data['espay']['qrUrl'];
       } else {
         print('Failed to login. Status code: ${response.statusCode}');
         print('Response body: ${response.body}');
